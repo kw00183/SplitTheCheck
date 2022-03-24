9644cabb1fecc89ace0ad9a4fe0346ba37022939
kw00183 <kw00183@cs6242-vm10.uwg.westga.edu>
2022-03-23 23:49:35 -0400

    update application stylesheet with search_radio_label; add in header and main to application html; add in root routes; update index html with rough code for search; update the form html with drop down for states with 2-characters; add in us_state helper method; change list line odd color in restaurants stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants, except: [:destroy]
+  root :to => 'restaurants#index'
+
+  get '/:restaurants/:id' => 'restaurants/id', controller: 'restaurants', action: 'id'
+  get '*path' => redirect('/')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
